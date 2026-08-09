ae9f2fb424c0dc58b71e49e456aedeb8d45f4860
uniblab <uniblab@hotmail.com>
2026-08-09 19:12:57 -0400

    strcmp

--- stdbuf/src/Command.cs
+++ stdbuf/src/Command.cs
@@ -66,7 +66,7 @@ public static class Command {
 				index++;
 				break;
 			}
-			if ( "-" == token || !token.StartsWith( '-', StringComparison.Ordinal ) ) {
+			if ( "-" == token || !token.StartsWith( "-", StringComparison.Ordinal ) ) {
 				break;
 			}
 
