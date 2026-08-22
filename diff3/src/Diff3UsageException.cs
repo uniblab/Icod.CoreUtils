dbe106c30f3ca41b5f2f05e3d7e8eb6370725d90
uniblab <uniblab@hotmail.com>
2026-08-22 08:02:19 -0400

    BALEETED!

--- diff3/src/Diff3UsageException.cs
+++
@@ -1,8 +0,0 @@
-namespace Icod.DiffUtils.Diff3;
-
-/// <summary>Represents invalid <c>diff3</c> command-line usage.</summary>
-internal sealed class Diff3UsageException : Exception {
-	/// <summary>Initializes a usage exception.</summary>
-	public Diff3UsageException( string message ) : base( message ) {
-	}
-}
