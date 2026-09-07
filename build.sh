b8b99c06330cd8bd2ead936f5d562a36a9164420
Timothy Bruce <uniblab@hotmail.com>
2026-09-07 06:28:49 -0400

    Fix local Unix package validation

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ build()
 test()
 {
     printf '\n=== Test ===\n'
-    dotnet test Icod.CoreUtils.sln  \
+    dotnet test Icod.CoreUtils.sln \
         -c Debug \
         --no-build
 }
@@ -33,13 +33,13 @@ pack()
     dotnet pack Icod.CoreUtils.sln \
         -c Debug \
         --no-build \
-	--output artifacts 
+        --output artifacts
 }
 
 validate()
 {
     printf '\n=== Validate ===\n'
-    ./.github/scripts/verify-release-package.sh artifacts Debug
+    pwsh -NoLogo -NoProfile -File packaging/VerifyLocalPackage.ps1 -ArtifactDirectory artifacts
 }
 
 case "${1-}" in
@@ -49,7 +49,7 @@ case "${1-}" in
         build
         test
         pack
-	validate
+        validate
         ;;
 
     clean)
